4989fbff06ab589e2a6e4ddc3b2a87aa47f09dbb
Kwon Leto <150549812+kwonleto@users.noreply.github.com>
2023-12-17 03:31:25 +0600

    Added Clear button, removed default grid code and reduced into makeGrid function by default

--- sketch.js
+++ sketch.js
@@ -1,26 +1,18 @@
+let inputSize = 16;
 const container = document.querySelector('.container');
-const size = Math.round((container.offsetWidth * container.offsetWidth) / 256);
 const btn = document.querySelector('.btn');
-
-for(i = 0; i < size; i++) {
-    const grid = document.createElement('div');
-    grid.classList.add('default');
-    container.appendChild(grid);
-    grid.addEventListener('mousemove', (e) => {
-        if(e.buttons == 1)
-        grid.style.cssText = "background-color: black;"
-    });
-}
+const clear = document.querySelector('.clear');
 
 btn.addEventListener('click', () => {
-    const newGridSize = prompt("Enter a new size between 10 to 100");
-    if (newGridSize < 10 || newGridSize > 100) {
+    inputSize = prompt("Enter a new size between 10 to 100");
+    if (inputSize < 10 || inputSize > 100) {
         alert('Out of Range');
         return;
     }
     clearGrid();
-    makeGrid(newGridSize);
+    makeGrid(inputSize);
 });
+
 
 function clearGrid() {
     container.innerHTML = '';
@@ -29,18 +21,25 @@ function clearGrid() {
 function makeGrid(size) {
     const newSize = Math.round((container.offsetWidth * container.offsetWidth) / (size * size));
     
-    for(j = 0; j < newSize; j++) {
-        const newGrid = document.createElement('div');
-        newGrid.style.width = size + 'px';
-        newGrid.style.height = size + 'px';
-        newGrid.classList.add('new');
-        container.appendChild(newGrid);
-        newGrid.addEventListener('mousemove', (e) => {
+    for(i = 0; i < newSize; i++) {
+        const grid = document.createElement('div');
+        grid.style.width = size + 'px';
+        grid.style.height = size + 'px';
+        grid.classList.add('new');
+        container.appendChild(grid);
+        grid.addEventListener('mousemove', (e) => {
             if(e.buttons == 1)
-            newGrid.classList.add('draw');
+            grid.classList.add('draw');
         });
     }
 } 
+
+makeGrid(inputSize);
+
+clear.addEventListener('click', () => {
+    clearGrid();
+    makeGrid(inputSize);
+});
 
 
 /*const draw = document.querySelectorAll('.default');
